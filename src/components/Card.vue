b4036a882902b22a7832555a2ff982bbcd6d160b
Kieran Mayer <kieran4mayer@gmail.com>
2023-09-16 16:43:33 +0930

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    title: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    image: {
+        type: String
+    },
+    stats:{
+        type: Object
+    }
+})
+
+const barValue = ref(`${props.stats[0].amount}px`)
+
+</script>
+
+<template>
+<div class="card">
+    <div class="card-header">
+      <h2>{{ title }}</h2>
+      <h5>{{ subtitle }}</h5>
+    </div>
+    <div class="card-body">
+        <img :src="image" />
+      <p>{{ description }}</p>
+      <div class="bar-chart-container">
+        <div class="bar-container">
+            <p>{{ stats[0].name }}</p>
+            <div class="bar" :style="{width: barValue}" ></div>
+
+        </div>
+      </div>
+    </div>
+</div>
+</template>
+
+
+<style>
+.card {
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  padding: 16px;
+  max-width: 300px;
+  margin: 16px;
+}
+
+.bar-chart-container {
+    display: flex;
+    align-items: flex-end;
+    background-color: #f9f9f9;
+    padding: 10px;
+    border: 1px solid #ccc;
+}
+
+.bar-container{
+    display: flex;
+    align-items: center;
+}
+
+.bar {
+    height: 10px;
+    margin: 0 5px;
+    max-width: 200px;
+    background-color: #76c7c0;
+ }
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,115 @@
+<script setup>
+import { ref } from 'vue';
+import Card from '../components/Card.vue';
+const initalItems = [
+  {
+    title: 'Driving',
+    subtitle: 'Driving 1 km in a standard vehicle',
+    description: 'The car is petrol powered and is an average 4 door sedan.',
+    image: '/car.jpg',
+    stats: [
+      {
+        name: "carbon",
+        amount: 280
+      },
+      {
+        name: "calories",
+        amount: 0
+      }      
+    ]
+  },
+  {
+    title: 'Riding a bike',
+    subtitle: 'Riding 1 km on a bike',
+    description: 'The bike is a standard road bike.',
+    image: 'biking.webp',
+    stats: [
+      {
+        name: "carbon",
+        amount: 1
+      },
+      {
+        name: "calories",
+        amount: 50
+      }      
+    ]
+  },
+  {
+    title: 'Driving an EV',
+    subtitle: 'Driving 1 km in a standard vehicle',
+    description: 'The electric vehicle is a Tesla Model 3.',
+    image: 'ev.jpg',
+    stats: [
+      {
+        name: "carbon",
+        amount: 30
+      },
+      {
+        name: "calories",
+        amount: 0
+      }      
+    ]
+  },
+  {
+    title: 'Eating a steak',
+    subtitle: 'Eating a 250gram steak',
+    description: 'The steak was from a grass fed farm.',
+    image: 'steak.jpg',
+    stats: [
+      {
+        name: "carbon",
+        amount: 280
+      },
+      {
+        name: "calories",
+        amount: 500
+      }      
+    ]
+  }
+]
+
+
+
+let items = ref(initalItems);
+
+const sortItems = () => {
+  items.value.sort((a, b) =>
+    a.stats.find(stat => stat.name === "carbon").amount - 
+    b.stats.find(stat => stat.name === "carbon").amount
+  );
+  console.log(items.value)
+}
+
+</script>
+
+<template>
+  <main class="home">
+    <h1>Home</h1>
+    <button class="button" @click="sortItems">Sort by carbon</button>
+
+    <div class="home-container">
+    <Card class="card-item" v-for="(item, index) in items"
+      :key="item.title"
+      :title="item.title"
+      :subtitle="item.subtitle"
+      :description="item.description"
+      :stats="item.stats"
+      :image="item.image"
+    />
+  </div>
+  </main>
+</template>
+
+<style>
+.home-container{
+  display: flex;
+  flex-grow: 1;
+  flex-wrap: wrap;
+}
+
+.card-item{
+  flex: 1 1 0;
+}
+
+
+</style>
